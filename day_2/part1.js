819b61f1b68877deb98c0df3c171b5939b1574a2
lurnie <lurnsp22@gmail.com>
2023-12-02 14:57:46 -0600

    Day 2

---
+++ day_2/part1.js
@@ -0,0 +1,26 @@
+const fs = require('fs'); const readLine = require('readline');
+const colorMaxes = {'red': 12, 'green': 13, 'blue': 14};
+
+function returnIDorZero(line) {
+    let splitValues = line.split(' ');
+    let id = splitValues[1];
+    id = Number(id.substring(0, id.length - 1));
+    for (let i = 2; i < splitValues.length; i += 2) {
+        // unless it's the last value of the last game, get rid of the final character of the color, since it will be , or ;
+        let color = (i + 2 < splitValues.length) ? splitValues[i + 1].slice(0, -1) : splitValues[i + 1];
+        if (splitValues[i] > colorMaxes[color]) {return 0;}
+    }
+    return id;
+}
+
+async function readLines() {
+    const dataStream = fs.createReadStream('input.txt');
+    const readingLine = readLine.createInterface(dataStream);
+
+    let sum = 0;
+    for await (let line of readingLine) {
+        sum += returnIDorZero(line);
+    }
+    console.log(sum);
+}
+readLines();
---
+++ day_2/part2.js
@@ -0,0 +1,25 @@
+const fs = require('fs'); const readLine = require('readline');
+
+function returnIDorZero(line) {
+    let splitValues = line.split(' ');
+    let colorMaxes = {'red': 0, 'green': 0, 'blue': 0};
+
+    for (let i = 2; i < splitValues.length; i += 2) {
+        // unless it's the last value of the last game, get rid of the final character of the color, since it will be , or ;
+        let color = (i + 2 < splitValues.length) ? splitValues[i + 1].slice(0, -1) : splitValues[i + 1];
+        if (Number(splitValues[i]) > colorMaxes[color]) {colorMaxes[color] = Number(splitValues[i]); }
+    }
+    return colorMaxes['red'] * colorMaxes['green'] * colorMaxes['blue'];
+}
+
+async function readLines() {
+    const dataStream = fs.createReadStream('input.txt');
+    const readingLine = readLine.createInterface(dataStream);
+
+    let sum = 0;
+    for await (let line of readingLine) {
+        sum += returnIDorZero(line);
+    }
+    console.log(sum);
+}
+readLines();
