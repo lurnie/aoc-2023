13a9bbd8e38dba224f81e1fb5bdd4249ebd6fb23
lurnie <lurnsp22@gmail.com>
2023-12-09 12:02:18 -0600

    Day 9

---
+++ day_9/part2.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString();
+let lines = input.split('\r\n');
+
+function extrapolate(array) {
+    let zero = true;
+    let differences = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== 0) {zero = false;} 
+        if (i + 1 < array.length) {differences.push(array[i + 1]-array[i]);}
+    }
+    if (zero) {return 0;}
+    return Number(array[0]) - extrapolate(differences);
+}
+
+let sum = 0;
+for (let line of lines) {
+    sum += extrapolate(line.split(' '));
+}
+console.log(sum);
